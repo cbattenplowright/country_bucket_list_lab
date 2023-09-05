360ecbb4eb0bfd903b24160d01a24bc72a64ffdc
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-05 14:45:55 +0100

    fetch data implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState(null);
+
+  const loadData = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    const jsonData = await response.json();
+    setCountries(jsonData);
+    console.log("loaded data");
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Country Bucket List</h1>
     </div>
   );
 }
